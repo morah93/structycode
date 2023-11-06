1e8c12ed19a6b8df7b94788a39af303d2c03b01a
Mohammed Rahman <97058803+morah93@users.noreply.github.com>
2023-11-05 19:23:59 -0500

    anagram

---
+++ anagram.js
@@ -0,0 +1,13 @@
+/*
+Write a function, anagrams, that takes in two strings as arguments.
+The function should return a boolean indicating whether or not the strings are anagrams.
+Anagrams are strings that contain the same characters, but in any order.
+*/
+
+const anagrams = (s1, s2) => {
+  // todo
+};
+
+module.exports = {
+  anagrams,
+};
