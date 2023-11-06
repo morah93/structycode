8ac74a01b23e5f2318c885e1914e98cb3f8f5db2
Mohammed Rahman <97058803+morah93@users.noreply.github.com>
2023-11-06 18:52:48 -0500

    compress refactored

--- compress.js
+++ compress.js
@@ -12,8 +12,57 @@ You can assume that the input only contains alphabetic characters.
 
 const compress = (s) => {
   // todo
+  let str = [];
+  let j = 0;
+  let i = 0;
+
+  while (j <= s.length) {
+    let count = 0;
+    if (s[j] === s[i]) {
+      j+=1
+    }else{
+      count = j - i
+      console.log(count)
+      if (count === 1){
+        str.push(s[i])
+      }else{
+        str.push(count,s[i])
+      }
+      i = j
+    }
+  }
+  return str.join('')
 };
 
 module.exports = {
   compress,
 };
+
+
+// const compress = (s) => {
+//   // todo
+//   let str = [];
+
+//   let j = 0;
+//   let i = 0;
+
+//   while (j < s.length) {
+//     if (s[j] === s[j+1]) {
+//       console.log(s[j], 'j')
+//       console.log(s[j+1], 'j+1')
+//       j+=1
+//     }else{
+//       let char = s.slice(i,j);
+//       console.log(char, 'char')
+//       let count = 0;
+//       for (let k = 0; k < char.length; k++){
+//         count+=1
+//         str.push(count,s[i])
+//       }
+//       j += 1
+//       i = j
+//     }
+//   }
+//   return str.toString()
+//   // return res.join('')
+// };
