aade0a228945cfef7d646cb8ec9db72508dbff6d
Mohammed Rahman <97058803+morah93@users.noreply.github.com>
2023-11-10 16:14:36 -0500

    anagram refactored

--- anagram.js
+++ anagram.js
@@ -6,6 +6,28 @@ Anagrams are strings that contain the same characters, but in any order.
 
 const anagrams = (s1, s2) => {
   // todo
+  let obj = {};
+  let newS1 = s1.toLowerCase()
+  let newS2 = s2.toLowerCase()
+
+  for (let char of newS1){
+    if (!(char in obj)){
+      obj[char] = 0
+    }
+    obj[char] += 1
+  }
+
+  for (char of newS2){
+   if(char in obj){
+     obj[char] -= 1
+   } else return false
+  }
+
+  for (let char in obj){
+    if (obj[char] != 0)return false
+  }
+
+  return true
 };
 
 module.exports = {
