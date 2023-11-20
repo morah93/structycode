090ec4b559a2de76f9731e93c02fb777f9823236
Mohammed Rahman <97058803+morah93@users.noreply.github.com>
2023-11-20 00:00:16 -0500

    intersection

---
+++ intersection.js
@@ -0,0 +1,8 @@
+// Write a function, intersection, that takes in two arrays, a,b, as arguments. The function should return a new array containing elements that are in both of the two arrays.
+
+// You may assume that each input array does not contain duplicate elements.
+
+
+const intersection = (a, b) => {
+  // todo
+};
