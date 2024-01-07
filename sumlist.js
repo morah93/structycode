a463735e595063619306eb5f89357e13f9136bb3
Mohammed Rahman <97058803+morah93@users.noreply.github.com>
2024-01-07 15:44:52 -0500

    sumlistref

--- sumlist.js
+++ sumlist.js
@@ -3,3 +3,14 @@ sum list
 Write a function, sumList, that takes in the head of a linked list containing numbers as an argument.
  The function should return the total sum of all values in the linked list.
 */
+
+// class Node {
+//   constructor(val) {
+//     this.val = val;
+//     this.next = null;
+//   }
+// }
+
+const sumList = (head) => {
+  // todo
+};
