1872b62b97eac343f425f441496512b0895dd544
Mohammed Rahman <97058803+morah93@users.noreply.github.com>
2024-01-07 15:47:53 -0500

    linklistfind

---
+++ linkedlistfind.js
@@ -0,0 +1,15 @@
+/*
+Write a function, linkedListFind, that takes in the head of a linked list and a target value.
+The function should return a boolean indicating whether or not the linked list contains the target.
+*/
+
+// class Node {
+//   constructor(val) {
+//     this.val = val;
+//     this.next = null;
+//   }
+// }
+
+const linkedListFind = (head, target) => {
+  // todo
+};
