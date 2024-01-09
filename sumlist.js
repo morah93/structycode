63b753d9b53caffe7ff328f4bd257f255743d60c
Mohammed Rahman <97058803+morah93@users.noreply.github.com>
2024-01-09 16:53:29 -0500

    sumlistrecursive

--- sumlist.js
+++ sumlist.js
@@ -13,4 +13,14 @@ Write a function, sumList, that takes in the head of a linked list containing nu
 
 const sumList = (head) => {
   // todo
+  // let sum = 0;
+  // let current = head;
+  // while (current !== null) {
+  //   sum += current.val
+  //   current = current.next
+  // }
+  // return sum
+
+  if (head === null) return 0;
+  return head.val + sumList(head.next);
 };
