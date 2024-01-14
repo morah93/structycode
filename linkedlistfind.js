530284bb41399df5c1ae41efc2607e85ae14ce40
Mohammed Rahman <97058803+morah93@users.noreply.github.com>
2024-01-14 14:58:04 -0500

    linklistfindrecursive

--- linkedlistfind.js
+++ linkedlistfind.js
@@ -10,13 +10,19 @@ The function should return a boolean indicating whether or not the linked list c
 //   }
 // }
 
+// const linkedListFind = (head, target) => {
+//   let current = head;
+//   while (current !== null){
+//     if (current.val === target) {
+//       return true
+//     }
+//     current = current.next
+//   }
+//   return false
+// };
+
 const linkedListFind = (head, target) => {
-  let current = head;
-  while (current !== null){
-    if (current.val === target) {
-      return true
-    }
-    current = current.next
-  }
-  return false
-};
+  if (head === null) return false
+  if (head.val === target) return true
+  return linkedListFind(head.next, target)
+}
