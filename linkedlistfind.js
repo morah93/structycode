12d520aa653ce2586d3fae9604a1a1a1eb0a7d72
Mohammed Rahman <97058803+morah93@users.noreply.github.com>
2024-01-14 14:55:26 -0500

    linklistfindsolu

--- linkedlistfind.js
+++ linkedlistfind.js
@@ -11,5 +11,12 @@ The function should return a boolean indicating whether or not the linked list c
 // }
 
 const linkedListFind = (head, target) => {
-  
+  let current = head;
+  while (current !== null){
+    if (current.val === target) {
+      return true
+    }
+    current = current.next
+  }
+  return false
 };
