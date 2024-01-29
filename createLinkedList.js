db75d7871b3498a8098352cffcbb8a5abaf0d04e
Mohammed Rahman <97058803+morah93@users.noreply.github.com>
2024-01-28 23:23:58 -0500

    createLinkedList

---
+++ createLinkedList.js
@@ -0,0 +1,15 @@
+/*
+Write a function, createLinkedList, that takes in an array of values as an argument.
+The function should create a linked list containing each element of the array as the values of the nodes.
+The function should return the head of the linked list.
+*/
+
+const createLinkedList = (values) => {
+  const dummyHead = new Node(null);
+  let tail = dummyHead;
+  for (let val of values) {
+    tail.next = new Node(val);
+    tail = tail.next;
+  }
+  return dummyHead.next;
+};
