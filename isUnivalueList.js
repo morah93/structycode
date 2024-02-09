5104841e240c3bfce912e00c1e9a46247323b4f8
Mohammed Rahman <97058803+morah93@users.noreply.github.com>
2024-02-08 22:35:21 -0500

    isUnivalueListRecursive

--- isUnivalueList.js
+++ isUnivalueList.js
@@ -7,13 +7,22 @@ You may assume that the input list is non-empty.
 
 */
 
-const isUnivalueList = (head) => {
-  let current = head;
+// const isUnivalueList = (head) => {
+//   let current = head;
 
-  while (current !== null) {
-    if (current.val !== head.val) return false;
-    current = current.next;
+//   while (current !== null) {
+//     if (current.val !== head.val) return false;
+//     current = current.next;
+//   }
+
+//   return true;
+// }
+
+const isUnivalueList = (head, prevVal = null) => {
+  if (head === null) return true;
+  if (prevVal === null || prevVal === head.val) {
+    return isUnivalueList(head.next, head.val);
+  } else {
+    return false;
   }
-
-  return true;
 }
