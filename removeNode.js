67056450d7f441bad8a211d57e1d7a7ab1e931c7
Mohammed Rahman <97058803+morah93@users.noreply.github.com>
2024-02-10 00:02:38 -0500

    removeNoderecursive

--- removeNode.js
+++ removeNode.js
@@ -11,19 +11,26 @@ You may assume that the input list is non-empty.
 You may assume that the input list contains the target.
 */
 
+// const removeNode = (head, targetVal) => {
+//   if (head.val === targetVal) return head.next;
+
+//   let current = head;
+//   let prev = null;
+//   while (current !== null) {
+//     if (current.val === targetVal) {
+//       prev.next = current.next;
+//       break;
+//     }
+//     prev = current;
+//     current = current.next;
+//   }
+
+//   return head;
+// };
+
 const removeNode = (head, targetVal) => {
+  if (head === null) return null;
   if (head.val === targetVal) return head.next;
-
-  let current = head;
-  let prev = null;
-  while (current !== null) {
-    if (current.val === targetVal) {
-      prev.next = current.next;
-      break;
-    }
-    prev = current;
-    current = current.next;
-  }
-
+  head.next = removeNode(head.next, targetVal);
   return head;
 };
