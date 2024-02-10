2a4a0340359589cf1c6973dffaece0aa3bb45f39
Mohammed Rahman <97058803+morah93@users.noreply.github.com>
2024-02-10 15:41:16 -0500

    createLinkedListrecursive

--- createLinkedList.js
+++ createLinkedList.js
@@ -4,12 +4,19 @@ The function should create a linked list containing each element of the array as
 The function should return the head of the linked list.
 */
 
-const createLinkedList = (values) => {
-  const dummyHead = new Node(null);
-  let tail = dummyHead;
-  for (let val of values) {
-    tail.next = new Node(val);
-    tail = tail.next;
-  }
-  return dummyHead.next;
+// const createLinkedList = (values) => {
+//   const dummyHead = new Node(null);
+//   let tail = dummyHead;
+//   for (let val of values) {
+//     tail.next = new Node(val);
+//     tail = tail.next;
+//   }
+//   return dummyHead.next;
+// };
+
+const createLinkedList = (values, i = 0) => {
+  if (i === values.length) return null;
+  const head = new Node(values[i]);
+  head.next = createLinkedList(values, i + 1);
+  return head;
 };
