4fd4a83792b00d21bb98bb3c37c6388974c1b211
Mohammed Rahman <97058803+morah93@users.noreply.github.com>
2024-02-10 15:16:07 -0500

    insertNoderecursive

--- insertNode.js
+++ insertNode.js
@@ -10,24 +10,42 @@ You may assume that the input list is non-empty and the index is not greater tha
 length of the input list.
 */
 
-const insertNode = (head, value, index) => {
+// const insertNode = (head, value, index) => {
+//   if (index === 0) {
+//     const newHead = new Node(value);
+//     newHead.next = head;
+//     return newHead;
+//   }
+
+//   let count = 0;
+//   let current = head;
+//   while (current !== null) {
+//     if (count === index - 1) {
+//       const next = current.next;
+//       current.next = new Node(value);
+//       current.next.next = next;
+//     }
+
+//     count += 1;
+//     current = current.next;
+//   }
+//   return head;
+// };
+
+const insertNode = (head, value, index, count = 0) => {
   if (index === 0) {
     const newHead = new Node(value);
     newHead.next = head;
     return newHead;
   }
 
-  let count = 0;
-  let current = head;
-  while (current !== null) {
-    if (count === index - 1) {
-      const next = current.next;
-      current.next = new Node(value);
-      current.next.next = next;
-    }
-
-    count += 1;
-    current = current.next;
+  if (count === index - 1) {
+    const next = head.next;
+    head.next = new Node(value);
+    head.next.next = next;
+    return head;
   }
+
+  insertNode(head.next, value, index, count + 1);
   return head;
 };
