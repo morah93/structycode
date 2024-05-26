3c957ad8cfebabb986b8e419f4466d0e836fe549
Mohammed Rahman <97058803+morah93@users.noreply.github.com>
2024-05-25 23:38:07 -0400

    factorial

---
+++ factorial.js
@@ -0,0 +1,8 @@
+// factorial
+// Write a function, factorial, that takes in a number n and returns the factorial of that number.
+// The factorial of n is the product of all the positive numbers less than or equal to n.You must solve this recursively.
+
+
+const factorial = (n) => {
+  // todo
+};
