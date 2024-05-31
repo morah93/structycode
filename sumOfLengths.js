d2bd0f2f135ee59ff73ef029a046cefa5f5d8976
Mohammed Rahman <97058803+morah93@users.noreply.github.com>
2024-05-30 23:46:18 -0400

    sumoflength

---
+++ sumOfLengths.js
@@ -0,0 +1,8 @@
+
+// sum of lengths
+// Write a function sumOfLengths that takes in array of strings and returns the total length of the strings.
+
+
+const sumOfLengths = (strings) => {
+  // todo
+};
