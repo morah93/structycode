d852440ea7f21943c446427a837722998d0d7c1b
Mohammed Rahman <97058803+morah93@users.noreply.github.com>
2024-06-01 19:34:45 -0400

    addLists

---
+++ addLists.js
@@ -0,0 +1,11 @@
+// add lists
+// Write a function, addLists, that takes in the head of two linked lists, each representing a number.
+// The nodes of the linked lists contain digits as values.The nodes in the input lists are reversed; this means that the least significant digit of the number is the head.
+// The function should return the head of a new linked listed representing the sum of the input lists.The output list should have its digits reversed as well.
+
+// You must do this by traversing through the linked lists once.
+
+
+const addLists = (head1, head2) => {
+  // todo
+};
