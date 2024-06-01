ce53bdb51856323d6a8b8605afa26d90278a1736
Mohammed Rahman <97058803+morah93@users.noreply.github.com>
2024-06-01 19:27:40 -0400

    palindrome

---
+++ palindrope.js
@@ -0,0 +1,11 @@
+
+
+// palindrome recursive
+// Write a function, palindome, that takes in a string and returns a boolean indicating whether or not the string is the same forwards and backwards.
+
+// You must solve this recursively.
+
+
+const palindrome = (s) => {
+  // todo
+};
